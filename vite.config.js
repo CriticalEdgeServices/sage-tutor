c3c1173a0449a0b69b4446b849cb8b1fcac9b738
CriticalEdge <130863587+CriticalEdgeServices@users.noreply.github.com>
2026-04-06 02:03:11 -0400

    Configure Rollup input options in vite.config.js
    Add Rollup input options for main and parent HTML files.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        parent: './public/parent.html',
+      }
+    }
   }
 })
