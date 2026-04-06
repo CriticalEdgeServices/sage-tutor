4346481aaec002707347783070ae238aa85de3d1
CriticalEdge <130863587+CriticalEdgeServices@users.noreply.github.com>
2026-04-06 01:20:21 -0400

    Refactor root component to conditionally render App or ParentDashboard

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ParentDashboard from './ParentDashboard.jsx'
+
+function Root() {
+  const isParent = window.location.pathname.startsWith('/parent');
+  return isParent ? <ParentDashboard /> : <App />;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
